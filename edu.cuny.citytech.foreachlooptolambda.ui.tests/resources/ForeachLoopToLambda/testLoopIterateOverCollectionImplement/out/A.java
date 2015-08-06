f9cdebc5d974e6bd0640855364ec2c77658fd06a
Md Arefin <arefin.shahrear@gmail.com>
2015-08-06 16:42:41 -0400

    update the #8 testCase implement over Collection

--- edu.cuny.citytech.foreachlooptolambda.ui.tests/resources/ForeachLoopToLambda/testLoopIterateOverCollectionImplement/out/A.java
+++ edu.cuny.citytech.foreachlooptolambda.ui.tests/resources/ForeachLoopToLambda/testLoopIterateOverCollectionImplement/out/A.java
@@ -1,11 +1,92 @@
-package collectionTestCase;
+package p;
 
-abstract class C implements java.util.Collection {
+import java.util.Collection;
+import java.util.Iterator;
+
+class C implements java.util.Collection {
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] toArray(Object[] a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(Object e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addAll(Collection c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
     //...
 }
 class A {
     void m() {
-        C c = null;
+        C c = new C ();
         for (Object obj : c)
             ;
     }
