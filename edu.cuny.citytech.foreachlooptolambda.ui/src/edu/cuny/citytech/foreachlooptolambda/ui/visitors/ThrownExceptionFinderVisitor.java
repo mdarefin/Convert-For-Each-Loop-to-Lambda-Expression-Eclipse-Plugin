ca79579a5e064df4b9d7eeb8e7ba3f6bd2e52a31
Md Arefin <arefin.shahrear@gmail.com>
2015-08-08 10:03:55 -0400

    #9 rename the class ThrownExceptionFinder to ThrownExceptionFinderVisitor as it conflicts with other internal class

--- edu.cuny.citytech.foreachlooptolambda.ui/src/edu/cuny/citytech/foreachlooptolambda/ui/visitors/ThrownExceptionFinderVisitor.java
+++ edu.cuny.citytech.foreachlooptolambda.ui/src/edu/cuny/citytech/foreachlooptolambda/ui/visitors/ThrownExceptionFinderVisitor.java
@@ -1,21 +1,28 @@
 package edu.cuny.citytech.foreachlooptolambda.ui.visitors;
 
 import java.beans.Statement;
+import java.util.List;
 
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.EnhancedForStatement;
 import org.eclipse.jdt.core.dom.ThrowStatement;
 import org.eclipse.jdt.core.dom.TryStatement;
 import org.eclipse.jdt.internal.compiler.lookup.BlockScope;
 
-public class ThrownExceptionFinder extends ASTVisitor {
+public class ThrownExceptionFinderVisitor extends ASTVisitor {
 
+	static EnhancedForStatementVisitor visitor = new EnhancedForStatementVisitor();
+	
 	//check if any statement is passing get the exception for that Block code 
-	public static void processThrownExceptions(Statement statement, BlockScope blockScope) {
+	public static void processThrownExceptions(Statement statement) { //BlockScope blockScope
 
 	}
 	//traverse through the ASTNode and check if the catch block get any exception
 	private static boolean tryCatchExcaption(TryStatement tryStatement) {
-
+		
+		List<TryStatement> tryStatementList = tryStatement.catchClauses();
+		tryStatementList.stream().forEach(s -> System.out.println(s));
+		
 		return false;
 	}
 	
@@ -24,5 +31,4 @@ public class ThrownExceptionFinder extends ASTVisitor {
 
 		return false;
 	}
-
 }
