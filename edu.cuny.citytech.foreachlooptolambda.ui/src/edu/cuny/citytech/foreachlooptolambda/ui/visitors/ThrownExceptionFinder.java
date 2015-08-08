e7df71b84844808a2fc945733e08d632582ceb9f
Md Arefin <arefin.shahrear@gmail.com>
2015-08-08 02:33:28 -0400

    #9 adding two more method

--- edu.cuny.citytech.foreachlooptolambda.ui/src/edu/cuny/citytech/foreachlooptolambda/ui/visitors/ThrownExceptionFinder.java
+++ edu.cuny.citytech.foreachlooptolambda.ui/src/edu/cuny/citytech/foreachlooptolambda/ui/visitors/ThrownExceptionFinder.java
@@ -3,12 +3,26 @@ package edu.cuny.citytech.foreachlooptolambda.ui.visitors;
 import java.beans.Statement;
 
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.ThrowStatement;
+import org.eclipse.jdt.core.dom.TryStatement;
 import org.eclipse.jdt.internal.compiler.lookup.BlockScope;
 
-public class ThrownExceptionFinder extends ASTVisitor{
-	
-	public static void processThrownExceptions(Statement statement, BlockScope blockScope){
-		
+public class ThrownExceptionFinder extends ASTVisitor {
+
+	//check if any statement is passing get the exception for that Block code 
+	public static void processThrownExceptions(Statement statement, BlockScope blockScope) {
+
+	}
+	//traverse through the ASTNode and check if the catch block get any exception
+	private static boolean tryCatchExcaption(TryStatement tryStatement) {
+
+		return false;
 	}
 	
+	//traverse through the ASTNode and check if the ThrowStatement block get any exception
+	private static boolean throwExcaption(ThrowStatement throwStatement) {
+
+		return false;
+	}
+
 }
