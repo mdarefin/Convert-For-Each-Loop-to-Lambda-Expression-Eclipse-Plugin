0417c9fb261b454fe8dbe553bd87a78e2c730421
Md Arefin <arefin.shahrear@gmail.com>
2015-08-07 20:12:29 -0400

    #28 undating testCase

--- edu.cuny.citytech.foreachlooptolambda.ui.tests/resources/ForeachLoopToLambda/testLoopImplementlIterable/in/A.java
+++ edu.cuny.citytech.foreachlooptolambda.ui.tests/resources/ForeachLoopToLambda/testLoopImplementlIterable/in/A.java
@@ -1,14 +1,20 @@
 package p;
 
+import java.util.Iterator;
+
 class C implements java.lang.Iterable {
+
+	@Override
+	public Iterator iterator() {
+		return null;
+	}
   //...
 }
 
 class A {
-  void m() {
-    C c = null; //this class implements java.lang.Iterable.
-    //should fail precondition
-    for (Object o : c)
-      ;
-  }
+	  void m() {
+	    C c = null; //this class implements java.lang.Iterable.
+	    //should fail precondition
+	    for (Object o : c);
+   }
 }
