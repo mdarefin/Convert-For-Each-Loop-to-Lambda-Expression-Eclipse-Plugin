990b963cbca0e1335d2f26ec74e43635e4727534
Md Arefin <arefin.shahrear@gmail.com>
2015-08-11 00:27:32 -0400

    #9 more work

--- edu.cuny.citytech.foreachlooptolambda.ui/src/edu/cuny/citytech/foreachlooptolambda/ui/visitors/ThrownExceptionFinderVisitor.java
+++ edu.cuny.citytech.foreachlooptolambda.ui/src/edu/cuny/citytech/foreachlooptolambda/ui/visitors/ThrownExceptionFinderVisitor.java
@@ -10,37 +10,84 @@
  *******************************************************************************/
 package edu.cuny.citytech.foreachlooptolambda.ui.visitors;
 
+import java.util.HashSet;
 import java.util.Stack;
 
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.Statement;
+import org.eclipse.jdt.core.dom.TryStatement;
+import org.eclipse.jdt.internal.codeassist.ThrownExceptionFinder;
 import org.eclipse.jdt.internal.compiler.lookup.BlockScope;
-import org.eclipse.jdt.internal.compiler.util.SimpleSet;
+import org.eclipse.jdt.internal.compiler.lookup.ReferenceBinding;
 
-@SuppressWarnings({"rawtypes", "unchecked"})
 public class ThrownExceptionFinderVisitor extends ASTVisitor {
-	
-	private SimpleSet thrownExceptions;
+
+	private HashSet thrownExceptions;
 	private Stack exceptionsStack;
-	private SimpleSet caughtExceptions;
-	private SimpleSet discouragedExceptions;
-	
+	private HashSet caughtExceptions;
+	private HashSet discouragedExceptions;
+
 	/**
-	 * Finds the thrown exceptions minus the ones that are already caught in previous statement.
-	 * Exception is already caught even if its super type is being caught. Also computes, separately,
-	 * a list comprising of (a)those exceptions that have been caught already and (b)those exceptions that are thrown
-	 * by the method and whose super type has been caught already. 
+	 * Finds the thrown exceptions minus the ones that are already caught in
+	 * previous statement. Exception is already caught even if its super type is
+	 * being caught. Also computes, separately, a list comprising of (a)those
+	 * exceptions that have been caught already and (b)those exceptions that are
+	 * thrown by the method and whose super type has been caught already.
+	 * 
 	 * @param satement
 	 * @param scope
 	 */
-	public void processThrownExceptions(Statement satement, BlockScope scope) {
-		this.thrownExceptions = new SimpleSet();
+	public void processThrownExceptions(Statement statement, BlockScope scope) {
+		this.thrownExceptions = new HashSet();
 		this.exceptionsStack = new Stack();
-		this.caughtExceptions = new SimpleSet();
-		this.discouragedExceptions = new SimpleSet();
-	//	satement.traverse(this, scope);
-	//	removeCaughtExceptions(satement, true /*remove unchecked exceptions this time*/);
+		this.caughtExceptions = new HashSet();
+		this.discouragedExceptions = new HashSet();
+		//statement.traverse(this, scope);
+		//emoveCaughtExceptions(tryStatement,true /* remove unchecked exceptions this time */);
+	}
+	
+	/**
+	 * Returns all the already caught exceptions in catch blocks, found by the call to
+	 * {@link ThrownExceptionFinder#processThrownExceptions(TryStatement, BlockScope)}
+	 * @return Returns an array of those exceptions that have been caught already in previous catch or
+	 * multi-catch blocks of the same try block. (Exceptions caught in inner try-catches are obtained via
+	 * {@link ThrownExceptionFinder#getDiscouragedExceptions()}.
+	 */
+	public ReferenceBinding[] getAlreadyCaughtExceptions() {
+		ReferenceBinding[] allCaughtExceptions = new ReferenceBinding[this.caughtExceptions.size()];
+		this.caughtExceptions.toArray(allCaughtExceptions);
+		return allCaughtExceptions;
+	}
+	
+	public ReferenceBinding[] getThrownUncaughtExceptions() {
+		ReferenceBinding[] result = new ReferenceBinding[this.thrownExceptions.size()];
+		this.thrownExceptions.toArray(result);
+		return result;
 	}
 
+	
+	private void acceptException(ReferenceBinding binding) {
+		if (binding != null && binding.isValidBinding()) {
+			this.thrownExceptions.add(binding);
+		}
+	}
+	
+	public boolean visit(Statement statement, BlockScope scope) {
+		this.exceptionsStack.push(this.thrownExceptions);
+		HashSet exceptionSet = new HashSet();
+		this.thrownExceptions = exceptionSet;
+		//statement.BLOCK.traverse(this, scope);
+
+		this.thrownExceptions = (HashSet)this.exceptionsStack.pop();
+
+		Object[] values = exceptionSet.toArray();
+		for (int i = 0; i < values.length; i++) {
+			if (values[i] != null) {
+				this.thrownExceptions.add(values[i]);
+			}
+		}
+		
+		return false;
+	}
 	
 }
